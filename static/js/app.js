b1c28e50d13e7ddaef5301410ec917b77a91dade
RaeAnnG <raeanng516@gmail.com>
2024-01-12 14:25:05 -0600

    M14 Challenge Homework Starter Code 1.12.24

---
+++ static/js/app.js
@@ -0,0 +1,97 @@
+//Use the D3 Library to read in the samples.json
+//const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+//Set Variables
+var option = "";
+var dataSet ;
+var demo;
+
+// Use init function
+function init() {
+
+    //Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("selDataset")
+
+    // Use D3 for the dropdown select element
+    const selector = d3.select("#selDataset");
+
+    // sampleNames.foreach((sample) => {
+    //     selector
+    //         .append("option")
+    //         ,text(sample)
+    //         .property("value", sample);
+    // });
+
+
+// Fetch the JSON data and fill the select options
+d3.json("samples.json").then((data) => {
+    const sampleNames = data.names;
+    //console.log('Data: ${data}');
+
+    //Print the data to the console
+    console.log(dataSet);
+
+    //Set up an array of id names
+    let names = data.names;
+
+    //Use for loop to collect sample names
+    // for (const sample of sampleNames) {
+    //     selector.append("option").text(sample).property("value", sample);
+    // }
+
+    names.forEach((sample) => {
+        //Append each name as an option in the dropdown menu
+        //Add the name to the html file as an option element
+        //dropdownMenu.append("option").text(dname).property("value", dname);
+        selector.append("option").text(sample).property("value", sample);
+    });
+
+    //Assign the first name to the name variable
+    let fname = names[0];
+
+    //Call the functions needed for the charts
+    // demo(dname);
+    // bar(dname);
+    // bubble(dname);
+    // gauge(dname);
+
+
+  });
+}
+
+//Initialize the dashboard
+init();
+
+//Create the bar chart
+function bar(selectedvalue) {
+    //Pull the json data
+    d3.json("samples.json").then((data) => {
+        console.log('Data: ${data}');
+
+        //Create an array of sample objects
+        let samples = data.samples;
+
+        //Filter the data where id = selected value
+        let filteredData = samples.filter((sample) => sample.id === selectedvalue);
+
+        //Assign the first object to the object variable
+        let object = filteredData[0];
+
+        //Trace the data for the horizontal bar chart
+        let trace = [{
+            //Slice the top 10 OTUs
+            x: object.sample_values.slice(0,10).reverse(),
+            y: object.otu_ids.slice(0,10).map((otu_id) => `OTU ${otu_id}`).reverse(),
+            text: object.otu_labels.slice(0,10).reverse(),
+            type: "bar",
+            marker: {
+                color: "rgb(166,172,237)"
+            },
+            orientation: "h"
+        }];
+
+        //Use Plotly to create the bar chart
+        Plotly.newPlot("bar", trace);
+    });
+}
+
+
